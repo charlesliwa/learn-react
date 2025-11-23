bed40ed8299ab9a0863d2137527eaa32b4b4d111
Charles <Charles.liwananda@gmail.com>
2025-11-23 00:25:55 -0700

    update

---
+++ 02 - data-driven react/travel journal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="../src/images/globe.png" alt="globe icon" />
+            <h1>My Future Travel Journal</h1>
+        </header>
+    )
+}
---
+++ 02 - data-driven react/travel journal/src/data/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://sporthelicopterscapetown.co.za/wp-content/uploads/2023/11/Table-Mountain-Cape-Town.jpg",
+            alt: "Table Mountain"
+        },
+        title: "Table Mountain",
+        country: "South Africa",
+        googleMapsLink: "https://maps.app.goo.gl/m5vtgY2tDtfUuX5o6",
+        dates: "10 Mar, 2021 - 20 Mar, 2021",
+        text: "Table Mountain is a flat-topped mountain overlooking Cape Town. Known for its stunning views, diverse wildlife, and iconic cableway, it is one of South Africa’s most famous natural landmarks."
+    },
+    {
+        id: 2, 
+        img: {
+            src: "https://res.cloudinary.com/tourhq/image/upload/c_fill,f_auto,fl_progressive,g_auto,h_900,q_auto:best,w_1800/jegub81lkfg1csrf05zu",
+            alt: "Bwindi Impenetrable Forest"
+        },
+        title: "Bwindi Impenetrable Forest",
+        country: "Uganda",
+        googleMapsLink: "https://maps.app.goo.gl/H2BEG34d6NLJAmK47",
+        dates: "05 Jul, 2021 - 15 Jul, 2021",
+        text: "Bwindi Impenetrable Forest, located in southwestern Uganda, is known for its mountain gorillas and dense rainforest. It is a UNESCO World Heritage Site and a top destination for eco-tourism."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://www.kenyasafari.com/images/masai-mara-saruni-mara-590x390.jpg",
+            alt: "Maasai Mara"
+        },
+        title: "Maasai Mara National Reserve",
+        country: "Kenya",
+        googleMapsLink: "https://maps.app.goo.gl/8QVs5B8b5MNDXJkM9",
+        dates: "01 Oct, 2021 - 18 Oct, 2021",
+        text: "The Maasai Mara is a large national reserve in southwestern Kenya, known for the Great Migration, diverse wildlife, and Maasai cultural heritage. It's one of Africa’s most iconic safari destinations."
+    },
+]
