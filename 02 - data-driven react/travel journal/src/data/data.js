ec5b750586bde9bca6de6245df0c4d3a3388f112
Charles <Charles.liwananda@gmail.com>
2025-11-23 18:04:58 -0700

    update

--- 02 - data-driven react/travel journal/src/data/data.js
+++ 02 - data-driven react/travel journal/src/data/data.js
@@ -35,4 +35,52 @@ export default [
         dates: "01 Oct, 2021 - 18 Oct, 2021",
         text: "The Maasai Mara is a large national reserve in southwestern Kenya, known for the Great Migration, diverse wildlife, and Maasai cultural heritage. It's one of Africa’s most iconic safari destinations."
     },
+    {
+        id: 4,
+        img: {
+            src: "https://res.cloudinary.com/enchanting/q_80,f_auto,c_fit,w_640,h_480/exodus-web/2024/09/shutterstock_1137913091.jpg",
+            alt: "Victoria Falls"
+        },
+        title: "Victoria Falls",
+        country: "Zambia",
+        googleMapsLink: "https://maps.app.goo.gl/k6mLXu2r6ArB1iZJ7",
+        dates: "12 Dec, 2021 - 28 Dec, 2021",
+        text: "Victoria Falls, located on the border of Zambia and Zimbabwe, is one of the largest and most famous waterfalls in the world. Known locally as 'Mosi-oa-Tunya,' meaning 'The Smoke That Thunders.'"
+    },
+    {
+        id: 5,
+        img: {
+            src: "https://images.squarespace-cdn.com/content/v1/5ab577a645776e9177108f75/d86481f0-34a1-494a-8ad3-4f1a1dfcf2e5/Great-Pyramid-ROST.png",
+            alt: "Pyramids of Giza"
+        },
+        title: "Pyramids of Giza",
+        country: "Egypt",
+        googleMapsLink: "https://maps.app.goo.gl/YycbfLjPVDq6ZWcQ9",
+        dates: "03 Jan, 2022 - 15 Jan, 2022",
+        text: "The Pyramids of Giza, built over 4,500 years ago, are among the most iconic historical structures on Earth. They are the last remaining wonder of the ancient world."
+    },
+    {
+        id: 6,
+        img: {
+            src: "https://yale-threesixty.transforms.svdcdn.com/production/Erg_Chebbi_Maroc_Wikimedia_web.jpg?w=1500&h=1500&q=80&auto=format&fit=clip&dm=1740245716&s=4fb85369e1f315e97301b3397fe29cd4",
+            alt: "Sahara Desert"
+        },
+        title: "Sahara Desert",
+        country: "Morocco",
+        googleMapsLink: "https://maps.app.goo.gl/D9T1awQkX7tw9PXb9",
+        dates: "10 Apr, 2022 - 25 Apr, 2022",
+        text: "The Sahara Desert is the largest hot desert in the world. Morocco's Sahara region is known for its golden dunes, camel trekking, and nomadic Berber culture."
+    },
+    {
+        id: 7,
+        img: {
+            src: "https://www.easytravel.co.tz/wp-content/uploads/2022/02/Stone-Town-Zanzibar-Tanzania.jpg",
+            alt: "Stone Town, Zanzibar"
+        },
+        title: "Stone Town",
+        country: "Tanzania",
+        googleMapsLink: "https://maps.app.goo.gl/ev1zHzXiKr2xR3bw6",
+        dates: "01 Aug, 2022 - 14 Aug, 2022",
+        text: "Stone Town, a UNESCO World Heritage site, is the cultural heart of Zanzibar. Known for its Swahili architecture, spice trade history, and coastal scenery."
+    }
 ]
---
+++ 03 - react state/chef-claude/README.md
@@ -0,0 +1,9 @@
+# Chef Claude
+
+## What we learned:
+
+### 01 Enent listeners
+### 02 State
+### 03 Conditional rendering
+### 04 Forms
+### 05 State management strategies
